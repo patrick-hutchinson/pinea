12b731c0a8888f3321a7c6f56f2ca8214d4378ab
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-10-14 11:39:03 +0200

    Neuer Abstand

--- app/src/app/(home)/HomePage.jsx
+++ app/src/app/(home)/HomePage.jsx
@@ -43,7 +43,7 @@ export default function Home({ pictureBrush, portfolios, features, periodical, a
         <PictureBrush images={pictureBrush.images} />
       </section>
 
-      <h3 style={{ marginBottom: "var(--margin)" }}>FEATURE</h3>
+      <h3 style={{ marginBottom: "36px" }}>FEATURE</h3>
       <div className={`blur_container ${styles.blur_container}`}>
         <section className={`${styles.section} ${styles.feature}`}>
           <Feature features={features} />
