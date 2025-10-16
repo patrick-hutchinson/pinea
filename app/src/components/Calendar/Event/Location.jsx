9176710d70e5c13127c794a7047e0aa787c8b6d5
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-10-16 12:42:26 +0200

    Updated Filter

--- app/src/components/Calendar/Event/Location.jsx
+++ app/src/components/Calendar/Event/Location.jsx
@@ -3,15 +3,16 @@ import styles from "../Calendar.module.css";
 import { downloadEvent } from "@/helpers/downloadEvent";
 import DownloadButton from "@/components/Calendar/Event/DownloadButton";
 
-import Icon from "@/components/Icon";
+const Location = ({ event }) => {
+  const isUpcomingOrCurrent = !event.endDate || new Date(event.endDate) >= new Date();
 
-const Location = ({ event }) => (
-  <div style={{ display: "flex", justifyContent: "space-between", zIndex: 2 }}>
-    <div>{`${event.museum}, ${event.city} (${event.country.cca2})`}</div>
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between", zIndex: 2 }}>
+      <div>{`${event.museum}, ${event.city} (${event.country.cca2})`}</div>
 
-    <DownloadButton onClick={() => downloadEvent(event)} />
-    {/* <Icon path="/icons/add-button.svg" className={styles.icon} onClick={() => downloadEvent(event)} /> */}
-  </div>
-);
+      {isUpcomingOrCurrent && <DownloadButton onClick={() => downloadEvent(event)} />}
+    </div>
+  );
+};
 
 export default Location;
