861af2c1a1299bea637c45330aa2967335bea43b
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-11-21 01:22:52 +0100

    Implemented Header Marquee

--- app/src/components/Header/Menu/DesktopMenu.jsx
+++ app/src/components/Header/Menu/DesktopMenu.jsx
@@ -1,15 +1,44 @@
 import Link from "next/link";
 
-import { motion, AnimatePresence } from "framer-motion";
 import { translate } from "@/helpers/translate";
 
 import FadePresence from "@/components/Animation/FadePresence";
-import TextMarquee from "@/components/TextMarquee/TextMarquee";
+
 import Text from "@/components/Text/Text";
 
 import styles from "../Header.module.css";
 
+import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
+
+import emblaStyles from "@/components/Carousel/Carousel.module.css";
+
 const DesktopMenu = ({ site }) => {
+  const Carousel = () => {
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, dragFree: true, dragResistance: 1 }, [
+      AutoScroll({
+        playOnInit: true,
+        stopOnInteraction: false, // <-- here
+        stopOnMouseEnter: false, // <— optional: keep scrolling even on hover
+        speed: 1,
+      }),
+    ]);
+
+    // Triple the date in case it is not long enough to fill the width of the screen
+
+    return (
+      <div className={`${emblaStyles.carousel_outer} embla`} ref={emblaRef}>
+        <div className={`${emblaStyles.carousel_inner} embla__container`}>
+          {[...Array(4)].map((_, index) => (
+            <li key={index} className={`${emblaStyles.slide} embla__slide`}>
+              <Text text={translate(site.menu_teaser)} className={styles.marquee} />
+            </li>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   console.log(site, "site");
 
   return (
@@ -64,7 +93,8 @@ const DesktopMenu = ({ site }) => {
       </div>
 
       <div className={styles.promo}>
-        <TextMarquee text={<Text text={translate(site.menu_teaser)} className={styles.marquee} />} isActive={true} />
+        {/* <TextMarquee text={<Text text={translate(site.menu_teaser)} className={styles.marquee} />} isActive={true} /> */}
+        <Carousel />
       </div>
     </FadePresence>
   );
