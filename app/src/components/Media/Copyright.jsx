9ca4796b858d9e6d23127f9913bd9d5a683fdca9
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-11-27 10:48:32 +0100

    Update Copyright.jsx

--- app/src/components/Media/Copyright.jsx
+++ app/src/components/Media/Copyright.jsx
@@ -6,7 +6,7 @@ const Copyright = ({ copyright = "", mediaWidth, activeElement, isActive, classN
   useEffect(() => {}, [mediaWidth]);
   return (
     // ⚠️ Added width: "calc(100% - 6px)", If breaks, add only on mobile
-    <div className={`${className} ${styles.copyright}`} typo="h5" style={{ height: "10px", width: "calc(100% - 0px)" }}>
+    <div className={`${className} ${styles.copyright}`} typo="h5" style={{ height: "10px", width: "calc(100% - 6px)" }}>
       <div className={styles.copyright_text} style={{ width: "100%", height: "100%" }}>
         <TextMarquee
           text={copyright}
