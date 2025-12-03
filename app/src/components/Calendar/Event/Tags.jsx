ad01f86f6b0c4d1b5faa614c45953bd23dd72fe2
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-12-03 11:54:01 +0100

    Update Tags.jsx

--- app/src/components/Calendar/Event/Tags.jsx
+++ app/src/components/Calendar/Event/Tags.jsx
@@ -3,15 +3,18 @@ import CropButton from "@/components/Media/CropButton";
 
 import Label from "@/components/Label/Label";
 import Icon from "@/components/Icon/Icon";
+import { StateContext } from "@/context/StateContext";
+import { useContext } from "react";
 
 const Tags = ({ event, setShowGallery }) => {
+  const { isMobile } = useContext(StateContext);
   const toggleGallery = () => {
     setShowGallery((prev) => !prev);
   };
 
   return (
     <div className={styles.tags}>
-      <CropButton className={styles.cropButton} />
+      {isMobile && <CropButton className={styles.cropButton} />}
       {event.recommendation && <Label className={styles.notice}>RECOMMENDED</Label>}
       {event.highlight?.hosted && <Label className={styles.notice}>HOSTED</Label>}
       {event.highlight?.pinned && <Label className={styles.notice}>PINNED</Label>}
