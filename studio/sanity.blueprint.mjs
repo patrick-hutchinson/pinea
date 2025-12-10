523e74fb53fab22c87a52efab9ac860563bfe8c0
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-12-10 16:12:04 +0100

    Update sanity.blueprint.mjs

--- studio/sanity.blueprint.mjs
+++ studio/sanity.blueprint.mjs
@@ -1,11 +1,21 @@
 import {defineBlueprint, defineDocumentFunction} from '@sanity/blueprints'
 
+import {documentEventHandler} from '@sanity/functions'
+
+export const handler = documentEventHandler(async ({context, event}) => {
+  console.log('📝 Newsletter function fired!')
+  console.log('Event object:', JSON.stringify(event, null, 2))
+})
+
 export default defineBlueprint({
   resources: [
-    // defineDocumentFunction({name: 'my-function'}),
     defineDocumentFunction({
       name: 'newsletter-function',
-      event: {on: ['create', 'update'], filter: "_type=='newsletter'", projection: '_id'},
+      event: {
+        on: ['create', 'update'],
+        filter: 'defined(_id)',
+        projection: '{_id}', // ← FIXED
+      },
     }),
   ],
 })
