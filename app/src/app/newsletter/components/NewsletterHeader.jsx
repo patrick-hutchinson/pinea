d88ad67dd563d8a2e3a733feaade05513b1be858
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-12-15 12:01:50 +0100

    Added Newsletter Header

--- app/src/app/newsletter/components/NewsletterHeader.jsx
+++ app/src/app/newsletter/components/NewsletterHeader.jsx
@@ -33,13 +33,8 @@ const NewsletterHeader = ({ site, newsletter }) => {
                 textDecoration: "none",
               }}
             >
-              Photography Intermedia Et Al.
+              Pinea Periodical
             </a>
-          </td>
-
-          {/* SPACER (acts as the old gap: 40px) */}
-          <td width="40" className="spacer" style={{ fontSize: "0", lineHeight: "0" }}>
-            &nbsp;
           </td>
 
           {/* RIGHT SIDE */}
@@ -47,78 +42,7 @@ const NewsletterHeader = ({ site, newsletter }) => {
             <table border="0" cellPadding="0" cellSpacing="0" style={{ border: "0" }}>
               <tbody>
                 <tr>
-                  {/* Contact links */}
-                  <td
-                    valign="bottom"
-                    className="footer-links-td"
-                    style={{
-                      fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif",
-                      fontSize: "13px",
-                      lineHeight: "15px",
-                      textAlign: "left",
-                      paddingRight: "20px",
-                      paddingBottom: "0px",
-                    }}
-                  >
-                    <a
-                      href="https://www.pinea-periodical.com/about"
-                      target="_blank"
-                      style={{
-                        color: "#fff",
-                        textDecoration: "none",
-                        display: "block",
-                        lineHeight: "15px",
-                      }}
-                    >
-                      Contact
-                    </a>
-                    <a
-                      href="https://www.pinea-periodical.com/imprint"
-                      target="_blank"
-                      style={{
-                        color: "#fff",
-                        textDecoration: "none",
-                        display: "block",
-                        lineHeight: "15px",
-                      }}
-                    >
-                      Impressum
-                    </a>
-                  </td>
-
-                  {/* Logo */}
-                  <td
-                    valign="bottom"
-                    className="footer-logo-td"
-                    style={{ padding: 0, margin: 0, position: "relative", width: "100%" }}
-                  >
-                    <a
-                      href="https://www.bmwkms.gv.at/"
-                      target="_blank"
-                      style={{
-                        textDecoration: "none",
-                        position: "absolute",
-                        right: "0px",
-                        bottom: "0px",
-                      }}
-                    >
-                      <img
-                        src={logoSrc}
-                        alt="Logo"
-                        border="0"
-                        style={{
-                          display: "block",
-                          height: "36px",
-                          lineHeight: "0",
-                          border: "0",
-                          margin: "0",
-                          padding: "0",
-                          position: "relative",
-                          bottom: "-3px",
-                        }}
-                      />
-                    </a>
-                  </td>
+                  <div>{newsletter.release}</div>
                 </tr>
               </tbody>
             </table>
