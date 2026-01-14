b02f6e1ed11f6f27e2fddc3136890eff1e146524
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-01-14 10:13:43 +0100

    Update NewsletterPage.jsx

--- app/src/app/newsletter/[slug]/NewsletterPage.jsx
+++ app/src/app/newsletter/[slug]/NewsletterPage.jsx
@@ -35,7 +35,7 @@ const NewsletterPage = ({ site, newsletter }) => {
           {/* {newsletter.portfolios && <NewsletterPortfolios />} */}
           {newsletter.news && (
             <div className="news" style={{ marginBottom: "150px" }}>
-              <h3 style={{ textAlign: "center" }}>News</h3>
+              <h3 style={{ textAlign: "center", fontWeight: "normal" }}>News</h3>
 
               {newsletter.news.map((item, index) => (
                 <NewsletterNewsElement
