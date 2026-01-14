722dbbaa7e9ba30be162720620dfa51c7e306f89
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-01-14 01:33:32 +0100

    Update NewsletterAnnouncements.jsx

--- app/src/app/newsletter/components/NewsletterAnnouncements.jsx
+++ app/src/app/newsletter/components/NewsletterAnnouncements.jsx
@@ -9,7 +9,14 @@ const NewsletterAnnouncements = ({ url }) => {
     >
       <tr>
         {/* IMAGE — 50% */}
-        <td width="50%" height="300" align="center" valign="top" style={{ padding: "0px", paddingRight: "2px" }}>
+        <td
+          className="na-image"
+          width="50%"
+          height="300"
+          align="center"
+          valign="top"
+          style={{ padding: "0px", paddingRight: "2px" }}
+        >
           <table width="100%" height="300" cellPadding="0" cellSpacing="0" role="presentation" style={{ border: 0 }}>
             <tr>
               <td align="center" valign="middle" style={{ padding: 0 }}>
@@ -26,64 +33,74 @@ const NewsletterAnnouncements = ({ url }) => {
         </td>
 
         {/* CARD 1 — 25% */}
-        <td
-          width="25%"
-          height="300"
-          align="center"
-          valign="top"
-          style={{ padding: "0px", paddingRight: "2px", height: "calc(50vw / 1.6)" }}
-        >
-          <table
-            width="100%"
-            height="100%"
-            cellPadding="0"
-            cellSpacing="0"
-            role="presentation"
-            style={{ background: "#000", border: 0 }}
+        <tr className="na-cards-wrapper">
+          <td
+            className="na-card"
+            width="25%"
+            height="300"
+            align="center"
+            valign="top"
+            style={{ padding: "0px", paddingRight: "2px", height: "calc(50vw / 1.6)" }}
           >
-            <tr>
-              <td
-                align="center"
-                valign="middle"
-                style={{
-                  color: "#fff",
-                  padding: "12px",
-                  padding: "0px",
-                  textAlign: "center",
-                }}
-              >
-                <h4 style={{ margin: 0 }}>Follow us on Instagram</h4>
-              </td>
-            </tr>
-          </table>
-        </td>
+            <table
+              width="100%"
+              height="100%"
+              cellPadding="0"
+              cellSpacing="0"
+              role="presentation"
+              style={{ background: "#000", border: 0 }}
+            >
+              <tr>
+                <td
+                  align="center"
+                  valign="middle"
+                  style={{
+                    color: "#fff",
+                    padding: "12px",
+                    padding: "0px",
+                    textAlign: "center",
+                  }}
+                >
+                  <h4 style={{ margin: 0 }}>Follow us on Instagram</h4>
+                </td>
+              </tr>
+            </table>
+          </td>
 
-        {/* CARD 2 — 25% */}
-        <td width="25%" align="center" valign="top" height="300" style={{ padding: "0px", height: "calc(50vw / 1.6)" }}>
-          <table
-            width="100%"
-            height="100%"
-            cellPadding="0"
-            cellSpacing="0"
-            role="presentation"
-            style={{ background: "#000", border: 0 }}
+          {/* CARD 2 — 25% */}
+          <td
+            className="na-card"
+            width="25%"
+            align="center"
+            valign="top"
+            height="300"
+            style={{ padding: "0px", height: "calc(50vw / 1.6)" }}
           >
-            <tr>
-              <td
-                align="center"
-                valign="middle"
-                style={{
-                  color: "#fff",
-                  //  padding: "12px",
-                  padding: "0px",
-                  textAlign: "center",
-                }}
-              >
-                <h4 style={{ margin: 0 }}>Become a Member</h4>
-              </td>
-            </tr>
-          </table>
-        </td>
+            <table
+              width="100%"
+              height="100%"
+              cellPadding="0"
+              cellSpacing="0"
+              role="presentation"
+              style={{ background: "#000", border: 0 }}
+            >
+              <tr>
+                <td
+                  align="center"
+                  valign="middle"
+                  style={{
+                    color: "#fff",
+                    //  padding: "12px",
+                    padding: "0px",
+                    textAlign: "center",
+                  }}
+                >
+                  <h4 style={{ margin: 0 }}>Become a Member</h4>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
       </tr>
     </table>
   );
