38d52ecc18cd8cc9d35dd9738e8c0e20af5f8de7
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-01-14 02:59:46 +0100

    Update NewsletterShowcase.jsx

--- app/src/app/newsletter/components/NewsletterShowcase.jsx
+++ app/src/app/newsletter/components/NewsletterShowcase.jsx
@@ -3,29 +3,46 @@ import Longcopy from "@/components/Longcopy/Longcopy";
 const NewsletterShowcase = ({ item }) => {
   console.log("showcase image:", item.image);
   return (
-    <div
-      className="newsletter-running-text"
-      style={{
-        padding: "50px 30px",
-        paddingBottom: "150px",
-        lineHeight: "1.4",
-        maxWidth: "600px",
-        margin: "0 auto",
-      }}
-    >
-      <img
-        src={item.image.url}
-        alt=""
-        border="0"
+    <div>
+      <div
+        className="newsletter-running-text"
         style={{
-          display: "block",
-          width: "100%",
-          maxWidth: "var(--width-longcopy)",
-          height: "auto",
+          padding: "50px 30px",
+          paddingBottom: "150px",
+          lineHeight: "1.4",
+          maxWidth: "600px",
           margin: "0 auto",
         }}
-      />
-      <Longcopy text={item.text} />
+      >
+        <h4
+          style={{
+            display: "block",
+            width: "var(--width-longcopy) !important",
+            maxWidth: "var(--max-width-longcopy)",
+            minWidth: "var(--min-width-longcopy)",
+            height: "auto",
+            margin: "0 auto",
+            marginBottom: "12px",
+          }}
+        >
+          Promotion
+        </h4>
+        <img
+          src={item.image.url}
+          alt=""
+          border="0"
+          style={{
+            display: "block",
+            width: "var(--width-longcopy) !important",
+            maxWidth: "var(--max-width-longcopy)",
+            minWidth: "var(--min-width-longcopy)",
+            height: "auto",
+            margin: "0 auto",
+            marginBottom: "12px",
+          }}
+        />
+        <Longcopy text={item.text} />
+      </div>
     </div>
   );
 };
