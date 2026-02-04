e4d8f4c2f054454289b034970eb739d28d9aeb95
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-04 14:39:47 +0100

    Added Blur to Periodical page

--- app/src/app/periodical/PeriodicalPage.jsx
+++ app/src/app/periodical/PeriodicalPage.jsx
@@ -13,47 +13,52 @@ import TextFigure from "@/components/Figure/TextFigure";
 import Button from "@/components/Buttons/Button";
 
 import styles from "./PeriodicalPage.module.css";
+import BlurContainer from "@/components/BlurContainer/BlurContainer";
+import PineaIcon from "@/components/PineaIcon/PineaIcon";
 
 const PeriodicalPage = ({ page }) => {
   console.log(page, "page");
 
   return (
     <main className={styles.main}>
-      <FilterHeader array={["COMING SOON"]} />
+      <FilterHeader array={["Coming Soon"]} />
 
-      <Satellite className={styles.satellite} behaviour={"expand"} media={page.gallery} />
+      <BlurContainer>
+        <Satellite className={styles.satellite} behaviour={"expand"} media={page.gallery} />
 
-      <MediaPair className={styles.mediaPair}>
-        <ShowcaseFigure
-          path="/memberships"
-          above={{ title: `ISBN: ${page.isbn}` }}
-          medium={page.gallery[1].medium}
-          below={{
-            title: convertToPlainText(translate(page.callout)),
-            subtitle: (
-              <Button className={styles.button} onClick={() => handleClick(translatedName, membership)}>
-                <div style={{ position: "relative", top: "0.5px" }}>Buy Soon</div>
-              </Button>
-            ),
-          }}
-          background={"black"}
-        />
+        <MediaPair className={styles.mediaPair}>
+          <ShowcaseFigure
+            path="/memberships"
+            above={{ title: `ISBN: ${page.isbn}` }}
+            medium={page.gallery[1].medium}
+            below={{
+              title: convertToPlainText(translate(page.callout)),
+              subtitle: (
+                <Button className={styles.button} onClick={() => handleClick(translatedName, membership)}>
+                  <div style={{ position: "relative", top: "0.5px" }}>Buy Soon</div>
+                </Button>
+              ),
+            }}
+            background={"black"}
+          />
 
-        <div className={styles.textFigure} style={{ position: "relative" }}>
-          <ComponentSlideshow>
-            {page.periodicalInfo.map((info) => {
-              const above = { title: convertToPlainText(translate(info.title)) };
-              const content = convertToPlainText(translate(info.text));
+          <div className={styles.textFigure} style={{ position: "relative" }}>
+            <ComponentSlideshow>
+              {page.periodicalInfo.map((info) => {
+                const above = { title: convertToPlainText(translate(info.title)) };
+                const content = convertToPlainText(translate(info.text));
 
-              console.log(content, "content");
-              return <TextFigure above={above} content={content} />;
-            })}
-          </ComponentSlideshow>
-        </div>
-        <div />
-      </MediaPair>
+                console.log(content, "content");
+                return <TextFigure above={above} content={content} />;
+              })}
+            </ComponentSlideshow>
+          </div>
+          <div />
+        </MediaPair>
 
-      <MediaCarousel className={styles.mediaCarousel} announcements={page.announcements} />
+        <MediaCarousel className={styles.mediaCarousel} announcements={page.announcements} />
+      </BlurContainer>
+      <PineaIcon className={styles.pineaIcon} />
     </main>
   );
 };
