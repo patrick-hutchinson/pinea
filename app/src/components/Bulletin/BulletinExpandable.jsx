7a8fff28ffafb01bbebf805b83833d417b4c32d8
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-05 16:50:43 +0100

    Updating BulletinExpandable Margin

--- app/src/components/Bulletin/BulletinExpandable.jsx
+++ app/src/components/Bulletin/BulletinExpandable.jsx
@@ -123,7 +123,7 @@ const BulletinExpandable = ({ bulletin, title, text, runningText, label, classNa
           <Icon path="/icons/dropdown-button.svg" className={`${styles.icon} ${styles.expandIcon}`} />
         </div>
       </div>
-      <h2 className={styles.text} style={{ marginBottom: "var(--paragraph-spacing-2)" }}>
+      <h2 className={styles.text}>
         <Text text={text} />
       </h2>
 
