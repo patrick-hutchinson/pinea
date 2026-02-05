722e9d705a6cf5823dd9f817b8fb8d1ac0019d45
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-05 16:20:51 +0100

    Updated Share Icon

--- app/src/components/Bulletin/Bulletin.jsx
+++ app/src/components/Bulletin/Bulletin.jsx
@@ -29,7 +29,6 @@ const Bulletin = ({ title, text, label, className, link }) => {
             textIndent: 0,
             left: 0,
             marginLeft: label ? "var(--margin)" : 0,
-            marginBottom: "var(--paragraph-spacing-2)",
           }}
         >
           <AnimationLink path={link}>
--- app/src/components/Bulletin/BulletinExpandable.jsx
+++ app/src/components/Bulletin/BulletinExpandable.jsx
@@ -123,7 +123,7 @@ const BulletinExpandable = ({ bulletin, title, text, runningText, label, classNa
           <Icon path="/icons/dropdown-button.svg" className={`${styles.icon} ${styles.expandIcon}`} />
         </div>
       </div>
-      <h2 className={styles.text} style={{ marginBottom: 0 }}>
+      <h2 className={styles.text} style={{ marginBottom: "var(--paragraph-spacing-2)" }}>
         <Text text={text} />
       </h2>
 
