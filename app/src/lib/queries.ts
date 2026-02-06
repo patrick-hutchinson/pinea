d2a890fca6448f00aa6af000388a3fd431c3b284
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-06 12:25:06 +0100

    Updated Membership Page

--- app/src/lib/queries.ts
+++ app/src/lib/queries.ts
@@ -127,7 +127,13 @@ export const homePageQuery = `*[_type=="homePage"][0]{
   },
 }`;
 
-export const searchableData = `*[_type in ["news", "openCall", "interview", "review", "spotOn", "portfolio", "contributor", "event"]]{
+export const searchableData = `*[_type in ["news", "openCall", "interview", "review", "spotOn", "portfolio", "contributor", "event"]
+  &&
+  (
+    (_type == "event" && duration.endDate >= now()) ||           // only future events
+    (_type in ["interview", "review", "spotOn", "portfolio", "contributor"]) // keep all others
+  )
+]{
   _id,
   _type,
   title,
