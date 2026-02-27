e694b7405c0b5ab589e6942adfc0258921c4d069
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-27 17:42:14 +0100

    Update PersonInfoBody.jsx

--- app/src/components/People/PersonInfoBody.jsx
+++ app/src/components/People/PersonInfoBody.jsx
@@ -21,7 +21,7 @@ const PersonInfoBody = ({ person, articles, classNameCell, className }) => {
       </div>
       <div className={styles.info_cell}>
         <div>
-          <Text text={translate(person.bioVoice)} />
+          <Text text={translate(person.bioVoice ?? person.bio)} />
         </div>
       </div>
     </div>
