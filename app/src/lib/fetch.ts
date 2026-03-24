54582a5cb90dcfee97082f47ccbd6ec1cc1f8e9c
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-03-24 17:47:54 +0100

    Update fetch.ts

--- app/src/lib/fetch.ts
+++ app/src/lib/fetch.ts
@@ -2,7 +2,7 @@ import { production } from "./client/production";
 import { preview } from "./client/preview";
 
 const isProduction = process.env.VERCEL_ENV === "production";
-const isPreview = process.env.VERCEL_URL === "preview.pinea-periodical.com";
+const isPreview = process.env.VERCEL_ENV === "preview";
 const isLocal = !process.env.VERCEL_ENV;
 
 export const getSanityClient = () => {
