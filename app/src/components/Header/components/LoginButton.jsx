6b25b81010ab85be117c455ff6a6cce88e1bd567
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-03-31 18:42:42 +0200

    Update LoginButton.jsx

--- app/src/components/Header/components/LoginButton.jsx
+++ app/src/components/Header/components/LoginButton.jsx
@@ -3,25 +3,12 @@ import { useRouter } from "next/navigation";
 
 import { isAuthEnabled } from "@/lib/runtimeFlags";
 
-const isProduction = process.env.VERCEL_ENV === "production";
-const isPreview = process.env.VERCEL_ENV === "preview";
-const isLocal = !process.env.VERCEL_ENV;
-
-export const getSanityClient = () => {
-  if (isProduction) return "production";
-  if (isPreview || isLocal) return "preview";
-
-  return "preview";
-};
-
-const client = getSanityClient();
-
 const LoginButton = ({ isMobile, showMenu }) => {
   const router = useRouter();
 
-  // if (!isAuthEnabled) {
-  //   return null;
-  // }
+  if (!isAuthEnabled) {
+    return null;
+  }
 
   return (
     <AnimatePresence mode="popLayout">
@@ -30,11 +17,6 @@ const LoginButton = ({ isMobile, showMenu }) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { duration: 0.5, delay: 1 } }}
           exit={{ opacity: 0, transition: { duration: 0.5, delay: 0 } }}
-          style={{
-            pointerEvents: client === "production" ? "none" : "all",
-            opacity: 0.4,
-            cursor: client === "production" ? "not-allowed" : "default",
-          }}
         >
           <motion.button onClick={() => router.push("/login")}>Log In</motion.button>
         </motion.div>
