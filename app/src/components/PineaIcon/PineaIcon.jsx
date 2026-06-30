0a0184678516433df223dca566c003e9f9ec860b
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-06-30 16:59:48 +0200

    Blur shop icon and tighten menu spacing

--- app/src/components/PineaIcon/PineaIcon.jsx
+++ app/src/components/PineaIcon/PineaIcon.jsx
@@ -1,9 +1,9 @@
 import styles from "./PineaIcon.module.css";
 
-const PineaIcon = ({ className, onClick, src = "/icons/pinea_rechteck.svg" }) => {
+const PineaIcon = ({ className, isShopIcon = false, onClick, src = "/icons/pinea_rechteck.svg" }) => {
   const classes = [className, styles.pineaIcon].filter(Boolean).join(" ");
 
-  return <img data-pinea-icon className={classes} onClick={onClick} src={src} />;
+  return <img data-pinea-icon data-shop-icon={isShopIcon ? true : undefined} className={classes} onClick={onClick} src={src} />;
 };
 
 export default PineaIcon;
--- app/src/components/PineaIcon/ShopIcon.jsx
+++ app/src/components/PineaIcon/ShopIcon.jsx
@@ -4,7 +4,7 @@ import styles from "./ShopIcon.module.css";
 const ShopIcon = ({ className, onClick, src = "/icons/SHOP.svg" }) => {
   const classes = [styles.shopIcon, className].filter(Boolean).join(" ");
 
-  return <PineaIcon className={classes} onClick={onClick} src={src} />;
+  return <PineaIcon className={classes} isShopIcon onClick={onClick} src={src} />;
 };
 
 export default ShopIcon;
